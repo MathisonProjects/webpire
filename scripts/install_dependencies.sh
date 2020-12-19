5d9f59daaaf6658dea52fb2ff1804f3268a01429
Jacob Mathison <jacob.mathison@advisorsexcel.com>
2020-12-19 16:35:00 -0600

    Quick test

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,7 @@
+sudo yum install httpd -y
+sudo service httpd start
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
 npm install -g aws-sdk pm2 dotenv
 npm install
