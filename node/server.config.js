d3c5dd76223e52ade2830a06d1831bac3aa24613
Jacob Mathison <jakethewizard04@gmail.com>
2020-12-20 16:14:49 -0600

    Fixing server config

--- node/server.config.js
+++ node/server.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [
         {
           name: "webpire-node",
-          script: "./node/server.js",
+          script: "/var/www/html/node/server.js",
           instances: 1,
           exec_mode: "cluster",
           watch: true,
