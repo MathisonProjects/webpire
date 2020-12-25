f25d5dcd17daa58de63c23fb556c3bef2c7e7981
Jacob Mathison <jacob.mathison@advisorsexcel.com>
2020-12-25 10:51:36 -0600

    Cors fix?

--- node/server.js
+++ node/server.js
@@ -2,16 +2,14 @@ const express = require('express')()
 const fs = require('fs')
 const server = require('http').createServer(express)
 const Dotenv = require('dotenv-webpack')
-const io = require('socket.io')(server, { 
-  handlePreflightRequest: (req, res) => {
-    const headers = {
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-        "Access-Control-Allow-Credentials": true
-    };
-    res.writeHead(200, headers);
-    res.end();
-  }
+const io = require('socket.io')(server, {
+  allowUpgrades: true,
+  transports: [ 'polling', 'websocket' ],
+  pingTimeout: 9000,
+  pingInterval: 3000,
+  cookie: 'mycookie',
+  httpCompression: true,
+  origins: '*:*'
 })
 const Plugins = require('./plugins')
 const env = process.env.STAGE ? process.env.STAGE : "development";
